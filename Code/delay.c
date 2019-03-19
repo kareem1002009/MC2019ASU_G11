ad096799e8584737638aa8f5956d326f67db7b0a
omar hesham <omarnasr3939@gmail.com>
2019-03-19 20:33:27 +0200

    update

---
+++ Code/delay.c
@@ -0,0 +1,24 @@
+void delay_micro(int n)
+{
+	int i,j;
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j= 0; j < 3; ++j)
+		{
+			/* code */
+		}
+	}
+
+}
+void delay_milli(int n)
+{
+	int i,j;
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j= 0; j < 3185; ++j)
+		{
+			/* code */
+		}
+	}
+
+}
---
+++ Code/delay.h
@@ -0,0 +1,9 @@
+#ifndef DELAY
+#define DELAY
+
+
+void delay_micro(int n);
+void delay_milli(int n);
+
+
+#endif
