9d4fb2931d7e07db93f3d24ad9219a4ac309a450
omar hesham <omarnasr3939@gmail.com>
2019-03-19 16:33:14 +0200

    update
    Co-Authored-By: fatma abdelmoniem mohamed <fatmaabdelmoniem@users.noreply.github.com>
    Co-Authored-By: Kareem Salah <kareem1002009@users.noreply.github.com>
    Co-Authored-By: moerfan97 <moerfan97@users.noreply.github.com>
    Co-Authored-By: omarnasreldeen <omarnasreldeen@users.noreply.github.com>

---
+++ Code/ports.h
@@ -0,0 +1,30 @@
+#ifndef PORTS
+#define PORTS
+
+
+#define PortA 0
+#define PortB 1
+#define PortC 2
+#define PortD 3
+#define PortE 4
+#define PortF 5
+
+#define Pin0 0
+#define Pin1 1
+#define Pin2 2
+#define Pin3 3
+#define Pin4 4
+#define Pin5 5
+#define Pin6 6
+#define Pin7 7
+
+#define mask0  0x01
+#define mask1  0x02
+#define mask2  0x04
+#define mask3  0x08
+#define mask4  0x10
+#define mask5  0x20
+#define mask6  0x40
+#define mask7  0x80
+
+#endif
