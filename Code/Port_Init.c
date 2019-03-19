eec8cc0fb295801822f80b0db6a0b3c6807b003c
omar hesham <omarnasr3939@gmail.com>
2019-03-19 16:19:36 +0200

    Create Port_Init.c

---
+++ Code/Port_Init.c
@@ -0,0 +1,74 @@
+#include"types.h"
+#include"tm4c123gh6pm.h"
+#include"ports.h"
+
+void Port_Init(uint8 port_index)
+{
+	int delay=0;
+	SYSCTL_RCGCGPIO_R |=(1<<port_index);
+			delay++;
+	switch(port_index){
+
+		case PortA :
+ GPIO_PORTA_LOCK_R =0x4c4f434b;
+ GPIO_PORTA_CR_R  =0xff;
+ GPIO_PORTA_DEN_R =0xff;
+ GPIO_PORTA_AFSEL_R =0x00;
+ GPIO_PORTA_AMSEL_R =0x00;
+ GPIO_PORTA_AFSEL_R=0X00;
+ GPIO_PORTA_PCTL_R=0X00;
+
+								break;
+		case PortB :
+	
+ GPIO_PORTB_LOCK_R =0x4c4f434b;
+ GPIO_PORTB_CR_R  =0xff;
+ GPIO_PORTB_DEN_R =0xff;
+ GPIO_PORTB_AFSEL_R =0x00;
+ GPIO_PORTB_AMSEL_R =0x00;
+ GPIO_PORTB_AFSEL_R=0X00;
+ GPIO_PORTB_PCTL_R=0X00;				break;
+		case PortC :
+	
+ GPIO_PORTC_LOCK_R =0x4c4f434b;
+ GPIO_PORTC_CR_R  =0xff;
+ GPIO_PORTC_DEN_R =0xff;
+ GPIO_PORTC_AFSEL_R =0x00;
+ GPIO_PORTC_AMSEL_R =0x00;
+ GPIO_PORTC_AFSEL_R=0X00;
+ GPIO_PORTC_PCTL_R=0X00;	 break;
+		case PortD :
+	
+ GPIO_PORTD_LOCK_R =0x4c4f434b;
+ GPIO_PORTD_CR_R  =0xff;
+ GPIO_PORTD_DEN_R =0xff;
+ GPIO_PORTD_AFSEL_R =0x00;
+ GPIO_PORTD_AMSEL_R =0x00;
+ GPIO_PORTD_AFSEL_R=0X00;
+ GPIO_PORTD_PCTL_R=0X00;	
+								break;
+		case PortE :
+	
+ GPIO_PORTE_LOCK_R =0x4c4f434b;
+ GPIO_PORTE_CR_R  =0xff;
+ GPIO_PORTE_DEN_R =0xff;
+ GPIO_PORTE_AFSEL_R =0x00;
+ GPIO_PORTE_AMSEL_R =0x00;
+ GPIO_PORTE_AFSEL_R=0X00;
+ GPIO_PORTE_PCTL_R=0X00;	
+								break;
+		case PortF :
+
+ GPIO_PORTF_LOCK_R =0x4c4f434b;
+ GPIO_PORTF_CR_R  =0xff;
+ GPIO_PORTF_DEN_R =0xff;
+ GPIO_PORTF_AFSEL_R =0x00;
+ GPIO_PORTF_AMSEL_R =0x00;
+ GPIO_PORTF_AFSEL_R=0X00;
+ GPIO_PORTF_PCTL_R=0X00;		break;
+		default:
+								break;
+
+	}
+
+}
